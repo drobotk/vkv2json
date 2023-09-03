6b79dc454adffc798ca3405b0194ab9284cc5f9f
drobotk <pawwwll@gmail.com>
2023-09-03 19:40:05 +0200

    Repo init, 0.1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# vkv2json.py
+
+CLI utility to convert Valve KeyValue files into JSON.
+
+```
+usage: vkv2json.py [-h] [--version] [-o FILE] FILE
+
+Convert Valve KeyValue files into JSON
+
+positional arguments:
+  FILE                 Input file
+
+options:
+  -h, --help           show this help message and exit
+  --version            show program's version number and exit
+  -o FILE, --out FILE  Output file
+```
---
+++ vkv2json.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+
+
+__version__ = "0.1.0"
+__all__ = ("parse",)
+
+
+def parse(text: str, i: int = 0) -> tuple[list, int]:
+    out = []
+    key = None
+    while i < len(text):
+        c = text[i]
+        if c == '"':
+            end = text.index('"', i + 1)
+            buf = text[i + 1 : end]
+            i = end + 1
+
+            if not key:
+                key = buf
+            else:
+                out.append({key: buf})
+                key = None
+        elif c == "{":
+            i += 1
+            assert key
+            o, i = parse(text, i)
+            out.append({key: o})
+            key = None
+        elif c == "}":
+            i += 1
+            break
+        else:
+            i += 1
+
+    return out, i
+
+
+def main():
+    # fmt: off
+    parser = argparse.ArgumentParser(description='Convert Valve KeyValue files into JSON')
+    parser.add_argument("--version",    action="version", version=__version__)
+    parser.add_argument("-o", "--out",  metavar="FILE", help="Output file")
+    parser.add_argument("file",         metavar="FILE", help="Input file")
+    # fmt: on
+
+    args = parser.parse_args()
+
+    with open(args.file) as f:
+        text = f.read()
+
+    outfile = args.out
+    if not outfile:
+        outfile = os.path.basename(args.file) + ".json"
+
+    out, _ = parse(text)
+    with open(outfile, "w") as f:
+        json.dump(out, f)
+
+
+if __name__ == "__main__":
+    main()
